7bcd5ddc423638cf228ab2dafac09c18a7b5eebb
RoninsWork <yurkaronin@gmail.com>
2023-02-13 18:16:01 +0300

    рефакторинг разметки и стилизации для мобильного меню + добавил отключение скрола страницы при открытом меню

--- js/parts/navigation-copy.js
+++ js/parts/navigation-copy.js
@@ -1,16 +1,17 @@
 const buttonMenu = document.querySelector('.button--menu');
-const headerMobile = document.querySelector('.header__mobile');
-const headerMobileNavigation = document.querySelector('.header__mobile-navigation');
+const headerMobile = document.querySelector('.mobile-nivigation');
+// const headerMobileNavigation = document.querySelector('.header__mobile-navigation');
 // const headerMobileSupport = document.querySelector('.header__mobile-support');
-const headerNavigationList = document.querySelector('.navigation-list');
+// const headerNavigationList = document.querySelector('.navigation-list');
 
 buttonMenu.addEventListener('click', function() {
   buttonMenu.classList.toggle('isActive');
   headerMobile.classList.toggle('isActive');
+  document.body.classList.toggle('mob-menu-open');
 
-  if (headerMobileNavigation.children.length === 0) {
-    headerMobileNavigation.appendChild(headerNavigationList.cloneNode(true));
-  }
+  // if (headerMobileNavigation.children.length === 0) {
+  //   headerMobileNavigation.appendChild(headerNavigationList.cloneNode(true));
+  // }
 
   // if (headerMobileSupport.children.length === 0) {
   //   headerMobileSupport.appendChild(headerSupportList.cloneNode(true));
