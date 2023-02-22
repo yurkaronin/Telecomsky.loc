9208dbc27f7c0fec28d657fd688289fad7085bcb
RoninsWork <yurkaronin@gmail.com>
2023-02-22 16:59:53 +0300

    Закончил рефакторинг кнопок с анимацией

--- js/parts/modals.js
+++ js/parts/modals.js
@@ -1,34 +1,34 @@
-// Get references to the necessary elements
+const dialogButtons = document.querySelectorAll('[data-dialog-button]');
 const overlay = document.querySelector('.overlay');
-const modals = document.querySelectorAll('.modal');
-const buttons = document.querySelectorAll('[data-dialog-button]');
-const closeButtons = document.querySelectorAll('.close-btn');
 
-// Show a modal when its corresponding button is clicked
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const dialogId = button.getAttribute('data-dialog-button');
-    const dialog = document.querySelector(`[data-dialog-modal='${dialogId}']`);
-    document.body.style.overflow = 'hidden';
-    dialog.classList.add('modal-show');
-    overlay.classList.add('overlay-show');
+if (dialogButtons && overlay) {
+  const modals = document.querySelectorAll('.modal');
+  const closeButtons = document.querySelectorAll('.close-btn');
+
+  dialogButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const dialogId = button.getAttribute('data-dialog-button');
+      const dialog = document.querySelector(`[data-dialog-modal='${dialogId}']`);
+      document.body.style.overflow = 'hidden';
+      dialog.classList.add('modal-show');
+      overlay.classList.add('overlay-show');
+    });
   });
-});
 
-// Hide a modal when the overlay or close button is clicked
-overlay.addEventListener('click', () => {
-  modals.forEach(modal => {
-    modal.classList.remove('modal-show');
-  });
-  overlay.classList.remove('overlay-show');
-  document.body.style.overflow = 'auto';
-});
-
-closeButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal');
-    document.body.style.overflow = 'auto';
-    modal.classList.remove('modal-show');
+  overlay.addEventListener('click', () => {
+    modals.forEach(modal => {
+      modal.classList.remove('modal-show');
+    });
     overlay.classList.remove('overlay-show');
+    document.body.style.overflow = 'auto';
   });
-});
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal');
+      document.body.style.overflow = 'auto';
+      modal.classList.remove('modal-show');
+      overlay.classList.remove('overlay-show');
+    });
+  });
+};
